4ca808abb7bc091ddcc63768fdd4faa5ae7ed3f5
GogaKhubashvili <gogaxubashvili@gmail.com>
2025-09-02 21:47:01 +0400

    finish components and design

--- src/app/add-section/add-section.ts
+++ src/app/add-section/add-section.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule, NgClass, NgStyle } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { TaskService } from '../shared/task-service';
+import { Section, TaskService } from '../shared/task-service';
 import { SingleTaskComponent } from '../single-task/single-task';
 
 @Component({
@@ -18,22 +18,36 @@ import { SingleTaskComponent } from '../single-task/single-task';
   styleUrls: ['./add-section.scss'],
 })
 export class AddSectionComponent {
+  // constructor(public tasksSrv: TaskService) {}
+
+  // onDragOver(e: DragEvent) {
+  //   this.tasksSrv.onDragOver(e);
+  // }
+
+  // dropToSection(e: DragEvent, sectionIndex: number) {
+  //   const idx = this.tasksSrv.getDropIndex(e, 'section', sectionIndex);
+  //   this.tasksSrv.dropTaskToSection(sectionIndex, idx);
+  // }
+
+  // addTaskToSection(index: number) {
+  //   this.tasksSrv.addTaskToSection(index);
+  // }
+
+  // onDragStartFromSection(taskIndex: number, sectionIndex: number) {
+  //   this.tasksSrv.onDragStartFromSection(taskIndex, sectionIndex);
+  // }
+
+  @Input() list!: Section;
+  @Input() sectionIndex!: number;
+
   constructor(public tasksSrv: TaskService) {}
 
-  onDragOver(e: DragEvent) {
-    this.tasksSrv.onDragOver(e);
-  }
-
-  dropToSection(e: DragEvent, sectionIndex: number) {
-    const idx = this.tasksSrv.getDropIndex(e, 'section', sectionIndex);
-    this.tasksSrv.dropTaskToSection(sectionIndex, idx);
-  }
-
-  addTaskToSection(index: number) {
-    this.tasksSrv.addTaskToSection(index);
-  }
-
-  onDragStartFromSection(taskIndex: number, sectionIndex: number) {
-    this.tasksSrv.onDragStartFromSection(taskIndex, sectionIndex);
+  dropToSection(event: DragEvent, sectionIndex: number) {
+    const dropIndex = this.tasksSrv.getDropIndex(
+      event,
+      'section',
+      sectionIndex
+    );
+    this.tasksSrv.dropTaskToSection(sectionIndex, dropIndex);
   }
 }
